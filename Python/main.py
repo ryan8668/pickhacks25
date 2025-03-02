f02aab2c58c40cd19ef957c22fcc43dc7a8abc28
Jacob Schroll <jacoblschroll@gmail.com>
2025-03-01 19:08:28 -0600

    Functional main loop

--- Python/main.py
+++ Python/main.py
@@ -1,13 +1,16 @@
 import cv2
 import platform
+import zmq
+import json
 
-import server.server as server
 import pose_estimation.movenet as movenet
 
 camera = cv2.VideoCapture(0)
 success, img = camera.read()
 
-socket = server.initialize_server()
+context = zmq.Context()
+socket = context.socket(zmq.PUSH)
+socket.bind("tcp://*:5555")
 
 platform = str(platform.platform()).upper()
 movenetPath = ""
@@ -18,8 +21,13 @@ else:
 
 interpreter, model_details = movenet.initialize_movenet(movenetPath)
 
-while success:
+while True:
+    success, img = camera.read()
+    if not success:
+        print("Failed to read from camera")
+        break
+
     new_img = cv2.resize(img, (256, 256))
     keypoints = movenet.keypoint_prediction(interpreter, model_details, new_img)
 
-    socket.send(keypoints.tobytes())    
+    socket.send_json(json.dumps(keypoints.tolist()))
